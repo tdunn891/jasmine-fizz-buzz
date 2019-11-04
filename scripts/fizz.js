c389775ffdd0d54587751388b8da0252a5172b2e
tdunn891 <thomas.dunn@uqconnect.edu.au>
2019-11-04 14:01:22 +0000

    first commit

---
+++ scripts/fizz.js
@@ -0,0 +1,12 @@
+fizz = function(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "FizzBuzz";
+  }
+  if (num % 5 === 0) {
+    return "Fizz";
+  }
+  if (num % 3 === 0) {
+    return "Buzz";
+  }
+  return num;
+};
